55c302b32e797d35c84a6af24193d9418c513182
Indranil Chandra <indranildchandra@gmail.com>
2019-09-22 01:13:41 +0530

    Fixed dlib object load bug and reduced inferencing time

--- src/gaze_estimator.py
+++ src/gaze_estimator.py
@@ -4,42 +4,42 @@ import numpy as np
 
 
 def estimateGaze(image, image_points):
-    size = image.shape
+	size = image.shape
 
-    # 3D model points.
-    model_points = np.array([
-                                (0.0, 0.0, 0.0),             # Nose tip
-                                (0.0, -330.0, -65.0),        # Chin
-                                (-225.0, 170.0, -135.0),     # Left eye left corner
-                                (225.0, 170.0, -135.0),      # Right eye right corne
-                                (-150.0, -150.0, -125.0),    # Left Mouth corner
-                                (150.0, -150.0, -125.0)      # Right mouth corner 
-                            ])
-
-
-    # Approximation of Camera internals
-    focal_length = size[1]
-    center = (size[1]/2, size[0]/2)
-    camera_matrix = np.array([
-                             [focal_length, 0, center[0]],
-                             [0, focal_length, center[1]],
-                             [0, 0, 1]], 
-                             dtype = "double"
-                            )
-    logging.info("Camera Matrix :\n {0}".format(camera_matrix))
-
-    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs)
-    logging.info("Rotation Vector:\n {0}".format(rotation_vector))
-    logging.info("Translation Vector:\n {0}".format(translation_vector))
+	# 3D model points.
+	model_points = np.array([
+								(0.0, 0.0, 0.0),             # Nose tip
+								(0.0, -330.0, -65.0),        # Chin
+								(-225.0, 170.0, -135.0),     # Left eye left corner
+								(225.0, 170.0, -135.0),      # Right eye right corne
+								(-150.0, -150.0, -125.0),    # Left Mouth corner
+								(150.0, -150.0, -125.0)      # Right mouth corner 
+							])
 
 
-    # Project a 3D point (0, 0, 1000.0) onto the image plane.
-    # We use this to draw a line sticking out of the nose
-    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+	# Approximation of Camera internals
+	focal_length = size[1]
+	center = (size[1]/2, size[0]/2)
+	camera_matrix = np.array([
+							 [focal_length, 0, center[0]],
+							 [0, focal_length, center[1]],
+							 [0, 0, 1]], 
+							 dtype = "double"
+							)
+	logging.info("Camera Matrix :\n {0}".format(camera_matrix))
 
-    noseImagePoint = ( int(image_points[0][0]), int(image_points[0][1]))
-    noseEndPointProjection2D = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+	dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+	(success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs)
+	logging.info("Rotation Vector:\n {0}".format(rotation_vector))
+	logging.info("Translation Vector:\n {0}".format(translation_vector))
 
-    return [noseImagePoint, noseEndPointProjection2D]
-    
+
+	# Project a 3D point (0, 0, 1000.0) onto the image plane.
+	# We use this to draw a line sticking out of the nose
+	(nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+	noseImagePoint = ( int(image_points[0][0]), int(image_points[0][1]))
+	noseEndPointProjection2D = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+	return [noseImagePoint, noseEndPointProjection2D]
+	
--- src/video_feed_launcher.py
+++ src/video_feed_launcher.py
@@ -3,13 +3,14 @@ import sys
 import os
 import glob
 import cv2
+import dlib
 import numpy as np
 from datetime import datetime
 from face_landmark_detector import detectFaceLandmark
 
 # Set these variables
-frameRate = 5.0 #in Frames per Second (FPS)
-frameResolution = (640, 480) #Set (980,720)) for higher resolution
+frameRate = 20.0 #in Frames per Second (FPS)
+frameResolution = (980, 720) #Set (980,720)) for higher resolution
 test_subject_name = "test"
 test_number = "1"
 output_directory_path = os.path.join(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../output"), test_subject_name), test_number) 
@@ -18,8 +19,14 @@ output_directory_path = os.path.join(os.path.join(os.path.join(os.path.dirname(o
 source_directory_path = os.path.dirname(os.path.realpath(__file__))
 output_directory_path = os.path.abspath(output_directory_path)
 if not os.path.exists(output_directory_path):
-    os.makedirs(output_directory_path)
+	os.makedirs(output_directory_path)
 
+script_directory_path = os.path.dirname(os.path.realpath(__file__)) # absolute dir the script is in
+shape_predictor_relative_path = "../resources/shape_predictor_68_face_landmarks.dat"
+shape_predictor_path = os.path.join(script_directory_path, shape_predictor_relative_path)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor_path)
 
 fileName = str(datetime.now()) #example -> '2011-05-03 17:45:35.177000'
 logFileName = os.path.join(output_directory_path, ((fileName.replace("-","_").replace(" ","-").replace(":","_")).split('.'))[0]) + ".log" #example -> path/yyyy_mm_dd-hh_mm_ss.log
@@ -43,21 +50,21 @@ try:
 	videoWriterObj2 = cv2.VideoWriter(outputFileName, cv2.VideoWriter_fourcc('M','J','P','G'), frameRate, frameResolution)
 
 	while cv2.waitKey(1) < 0:
-	    has_frame, frame = cap.read()
+		has_frame, frame = cap.read()
 
-	    if not has_frame: #(keyPressed == ord('q')) or (keyPressed == ord('Q'))
-	    	logging.info("No frame returned...")
-	    	logging.info("Application Terminated!")
-	    	logging.info("----------------------------------------------------------------")
-	    	logging.info("----------------------------------------------------------------")
-	    	break
+		if not has_frame: #(keyPressed == ord('q')) or (keyPressed == ord('Q'))
+			logging.info("No frame returned...")
+			logging.info("Application Terminated!")
+			logging.info("----------------------------------------------------------------")
+			logging.info("----------------------------------------------------------------")
+			break
 
-	    videoWriterObj1.write(frame)
-	    outputImage = detectFaceLandmark(frame)
-	    videoWriterObj2.write(outputImage)
-	    logging.info("Captured Frame and Output Image written to Video file")
-	    logging.info("----------------------------------------------------------------")
-	    logging.info("----------------------------------------------------------------")
+		videoWriterObj1.write(frame)
+		outputImage = detectFaceLandmark(frame, detector, predictor)
+		videoWriterObj2.write(outputImage)
+		logging.info("Captured Frame and Output Image written to Video file")
+		logging.info("----------------------------------------------------------------")
+		logging.info("----------------------------------------------------------------")
 except Exception as e:
 	logging.error("An Exception occured!")
 	logging.error(str(e))
